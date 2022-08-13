86cb3018ef002768faaed9c932013a4df257fd58
Alan <al.limerick@gmail.com>
2022-08-13 22:25:17 +0100

    New mobile navigation

--- main.js
+++ main.js
@@ -7,3 +7,49 @@ navToggle.addEventListener("click", () => {
     : navToggle.setAttribute("aria-expanded", true);
   primaryNav.toggleAttribute("data-visible");
 });
+
+const mainMenu = document.querySelector(".main-menu");
+const subMenuAbout = document.querySelector(".sub-menu-about");
+const subMenuCommunity = document.querySelector(".sub-menu-community");
+const subMenuExperiences = document.querySelector(".sub-menu-experiences");
+
+const mainMenuItemAbout = document.querySelector(".main-about");
+const mainMenuItemCommunity = document.querySelector(".main-community");
+const mainMenuItemExperiences = document.querySelector(".main-experiences");
+
+const subMenuItemAbout = document.querySelector(".sub-about");
+const subMenuItemCommunity = document.querySelector(".sub-community");
+const subMenuItemExperiences = document.querySelector(".sub-experiences");
+
+mainMenuItemAbout.addEventListener("click", () => {
+  hideContent(mainMenu);
+  showContent(subMenuAbout);
+});
+mainMenuItemCommunity.addEventListener("click", () => {
+  hideContent(mainMenu);
+  showContent(subMenuCommunity);
+});
+mainMenuItemExperiences.addEventListener("click", () => {
+  hideContent(mainMenu);
+  showContent(subMenuExperiences);
+});
+subMenuItemAbout.addEventListener("click", () => {
+  hideContent(subMenuAbout);
+  showContent(mainMenu);
+});
+subMenuItemCommunity.addEventListener("click", () => {
+  hideContent(subMenuCommunity);
+  showContent(mainMenu);
+});
+subMenuItemExperiences.addEventListener("click", () => {
+  hideContent(subMenuExperiences);
+  showContent(mainMenu);
+});
+
+function hideContent(parent) {
+  parent.setAttribute("hidden", true);
+}
+
+function showContent(parent) {
+  parent.removeAttribute("hidden");
+}
